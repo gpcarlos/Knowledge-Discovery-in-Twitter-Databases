570e990201e5fac5092f66233768b3c59097c1e6
Carlos Gallardo Polanco <carlosgallardopolanco@gmail.com>
2017-05-27 20:16:51 +0200

    Bajada implementada

--- DropboxDemo.py
+++ DropboxDemo.py
@@ -1,5 +1,6 @@
 import dropbox
 import tempfile
+import shutil
 from TokenDropbox import token
 
 dbx = dropbox.Dropbox(token)
@@ -9,7 +10,16 @@ user = dbx.users_get_current_account()
 with open("twits.txt", "rb") as f:
     data = f.read()
 
+# Subida
 print("Subiendo")
 fname = "/datos_remotos.txt"
 dbx.files_upload(data, fname, mute=True)
 print("Subida Finalizada")
+
+# Bajada
+print("Descargando")
+path = "/datos_remotos.txt"
+name = "twitsDescargados.txt"
+file_temp = open(name)
+dbx.files_download_to_file(file_temp.name, path)
+print("Descarga Finalizada")
