66a9af3457655d8a4846006ae328a4783be0434c
Carlos Gallardo Polanco <carlosgallardopolanco@gmail.com>
2017-05-28 23:47:04 +0200

    Descarga en archivos diferentes

--- Servidor.py
+++ Servidor.py
@@ -5,64 +5,57 @@ import matplotlib.pyplot as plt
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-import json
 import re
 import dataset
-from Token_Twitter import consumer_key, consumer_secret, access_key, access_secret
-
 import time
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_key, access_secret)
-api = tweepy.API(auth)
-
-from threading import Thread
+from threading import Thread, current_thread
 from queue import *
-
 import zmq
 import json
 
-class StdOutListener(StreamListener):
-    def on_data(self, data):
-        try:
-            str123='twitter2.json'
-            with open(str123, 'a') as f:
-                #print(data)
-                f.write(data)
-                return True
-        except BaseException as e:
-            print("Error on_data: %s" % str(e))
-
-    def on_error(self, status):
-        print (status)
-
-
-class Worker(Thread):
-    def __init__(self, queue, name, hashtag, identify):
-        self.hashtag=hashtag
-        self.identify=identify
-        self.queue=queue
-        Thread.__init__(self, name=name)
-    def run(self):
-        l = StdOutListener()
-        stream = Stream(auth, l)
-        stream.filter(track=self.hashtag)
-        self.time.sleep(10) #tiempo en segundos
-        stream.disconnect()
-        print("holis")
-        self.queue.task_done()
+from Token_Twitter import consumer_key, consumer_secret, access_key, access_secret
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://127.0.0.1:1024")
 
+class StdOutListener(StreamListener):
+    def on_data(self, data):
+        try:
+            namefile=current_thread().name+".json"
+            with open(namefile, 'a') as f:
+                #print(data)
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+    def on_error(self, status):
+        print (status)
+
+class Worker(Thread):
+    def __init__(self, queue, hashtag):
+        self.hashtag=hashtag
+        self.queue=queue
+        Thread.__init__(self, name=hashtag)
+    def run(self):
+        l = StdOutListener()
+        stream = Stream(auth, l)
+        stream.filter(track=self.hashtag)
+        #self.time.sleep(10) #tiempo en segundos
+        #stream.disconnect()
+        self.queue.task_done()
+
+#MAIN
 vector = socket.recv_json()
 print(vector)
 nThreads=len(vector)
 try:
     q = Queue(nThreads)
     for i in range(nThreads):
-        t = Worker(q,"T"+str(i+1),vector[i],i)
+        t = Worker(q,vector[i])
         t.start()
         q.put(t)
     q.join()
